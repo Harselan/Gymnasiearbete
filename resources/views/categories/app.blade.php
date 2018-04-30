90ddc5373f72b56607f08377b7f3516eab5989c2
Harselan <haeressel@gmail.com>
2018-04-30 13:13:10 +0200

    Har uppdaterat datan man får ut av matcher för varje spel

--- resources/views/categories/app.blade.php
+++ resources/views/categories/app.blade.php
@@ -2,68 +2,85 @@
 
 @section( 'content' )
         <div class="col-md-10 mt-5 col-centered row text-center">
-            <div class="card col-md m-3">
+            <div class="card p-0 col-md m-3">
                 <div class="card-body">
                     <h4 class="card-title"><a>Kommande matcher</a></h4>
-                    <div class="table-responsive card-body">
+                    <div class=" card-body">
+                        <?php $counter = 0; ?>
                         @foreach( $matches['upcoming'] as $key => $match )
                             @if( isset( $match->opponents[1] ) && !empty( $match->opponents[1] ) && $key < 10 )
                             <div class="row">
                                 <div class="col-md p-0">
                                     <img style="width:20px;" src="{{  $match->opponents[0]->opponent->image_url }}" alt="">
-                                    <p class="small text-sm">{{ $match->opponents[0]->opponent->acronym }}</p>
+                                    <p>{{ $match->opponents[0]->opponent->acronym }}</p>
                                 </div>
 
                                 <div class="col-md p-0">
                                     <img style="width:20px;">
-                                    <p class="small text-sm" style="height:20px;">MOT</p>
+                                    <p style="height:20px;">MOT</p>
+                                    <a href="#match{{ $counter }}" data-toggle="collapse">Se mer</a>
                                 </div>
 
                                 <div class="col-md p-0">
                                     <img style="width:20px" src="{{  $match->opponents[1]->opponent->image_url }}" alt="">
-                                    <p class="small text-sm">{{ $match->opponents[1]->opponent->acronym }}</p>
+                                    <p>{{ $match->opponents[1]->opponent->acronym }}</p>
+                                </div>
+
+                                <div id="match{{ $counter }}" class="collapse text-left col-md-12">
+                                    <small><b>Datum:</b> {{ date( 'Y-m-d', strtotime( $match->begin_at ) ) }}</small><br>
+                                    <small><b>Tid:</b> {{ date( 'H:i', strtotime( $match->begin_at ) ) }}</small><br>
+                                    <small><b>Serie:</b> {{ $match->serie->slug }}</small>
                                 </div>
                             </div>
+                            <?php $counter++; ?>
                             @endif
                         @endforeach
                     </div>
                 </div>
             </div>
-            <div class="card col-md m-3">
+            <div class="card p-0 col-md m-3">
                 <div class="card-body">
                     <h4 class="card-title"><a>Pågående matcher</a></h4>
-                    <div class="table-responsive card-body">
+                    <div class=" card-body">
                         @foreach( $matches['running'] as $key => $match )
                             @if( isset( $match->opponents[1] ) && !empty( $match->opponents[1] ) && $key < 10 )
                             <div class="row">
                                 <div class="col-md p-0">
                                     <img style="width:20px;" src="{{  $match->opponents[0]->opponent->image_url }}" alt="">
-                                    <p class="small text-sm">{{ $match->opponents[0]->opponent->acronym }}</p>
+                                    <p>{{ $match->opponents[0]->opponent->acronym }}</p>
                                 </div>
 
                                 <div class="col-md p-0">
                                     <img style="width:20px;">
-                                    <p class="small text-sm" style="height:20px;">MOT</p>
+                                    <p style="height:20px;">MOT</p>
+                                    <a href="#match{{ $counter }}" data-toggle="collapse">Se mer</a>
                                 </div>
 
                                 <div class="col-md p-0">
                                     <img style="width:20px" src="{{  $match->opponents[1]->opponent->image_url }}" alt="">
-                                    <p class="small text-sm">{{ $match->opponents[1]->opponent->acronym }}</p>
+                                    <p>{{ $match->opponents[1]->opponent->acronym }}</p>
+                                </div>
+
+                                <div id="match{{ $counter }}" class="collapse text-left col-md-12">
+                                    <small><b>Datum:</b> {{ date( 'Y-m-d', strtotime( $match->begin_at ) ) }}</small><br>
+                                    <small><b>Tid:</b> {{ date( 'H:i', strtotime( $match->begin_at ) ) }}</small><br>
+                                    <small><b>Serie:</b> {{ $match->serie->slug }}</small>
                                 </div>
                             </div>
+                            <?php $counter++; ?>
                             @endif
                         @endforeach
                     </div>
                 </div>
             </div>
-            <div class="card col-md m-3">
+            <div class="card p-0 col-md m-3">
                 <div class="card-body">
                     <h4 class="card-title"><a>Avslutade matcher</a></h4>
-                    <div class="table-responsive card-body">
+                    <div class=" card-body">
                         @foreach( $matches['past'] as $key => $match )
                             @if( isset( $match->opponents[1] ) && !empty( $match->opponents[1] ) && $key < 10 )
                                 <img style="width:20px;" src="{{  $match->winner->image_url }}" alt="">
-                                <p class="small text-sm">{{ $match->winner->name }} <span class="bg-success">Vann</span></p>
+                                <p>{{ $match->winner->name }} <span class="bg-success">Vann</span></p>
                             @endif
                         @endforeach
                     </div>
