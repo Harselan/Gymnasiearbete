871d92fef60c9abc8485236973f0d1eb17ec4ad0
Harselan <haeressel@gmail.com>
2018-04-30 16:50:40 +0200

    Har gjort lite småfix för titeln och ikonen som är där

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <meta name="author" content="Harald Selegård">
+    <link rel="icon" href="{{ asset( 'img/icons/title-icon.png' ) }}">
 
-    <title>Carousel Template for Bootstrap</title>
+    <title>Game library</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
